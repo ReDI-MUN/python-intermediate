17251a8015705de9d9138461fe7521afb62ec070
Cooper Siyag <cooper.siyag@workday.com>
2023-09-26 19:55:52 +0200

    Added exercise.py

---
+++ lessons/5 - Map Filter Reduce/exercise.py
@@ -0,0 +1,70 @@
+# -------------- MAP EXERCISES --------------
+# Exercise 1: Double Then Square
+def double_then_square(numbers):
+    # TODO: Implement this function
+    pass
+
+
+input1 = [1, 2, 3]
+output1 = []  # TODO: Add your output here
+assert double_then_square(input1) == output1
+
+
+# Exercise 2: Reverse Strings
+def reverse_strings(strings):
+    # TODO: Implement this function
+    pass
+
+
+input2 = ['apple', 'banana']
+output2 = []  # TODO: Add your output here
+assert reverse_strings(input2) == output2
+
+
+# -------------- FILTER EXERCISES --------------
+# Exercise 3: Filter Strings with Vowels
+def filter_strings_with_vowels(strings):
+    # TODO: Implement this function
+    pass
+
+
+input3 = ['sky', 'apple', 'try']
+output3 = []  # TODO: Add your output here
+assert filter_strings_with_vowels(input3) == output3
+
+
+# Exercise 4: Filter Multiples of N
+def filter_multiples_of_n(numbers, n):
+    # TODO: Implement this function
+    pass
+
+
+input4_numbers = [10, 15, 20, 25]
+input4_n = 5
+output4 = []  # TODO: Add your output here
+assert filter_multiples_of_n(input4_numbers, input4_n) == output4
+
+
+# -------------- REDUCE EXERCISES --------------
+# Exercise 5: Find Maximum Length
+def find_maximum_length(strings):
+    # TODO: Implement this function
+    pass
+
+
+input5 = ['apple', 'banana', 'cherry']
+output5 = None  # TODO: Add your output here
+assert find_maximum_length(input5) == output5
+
+
+# Exercise 6: Sum of Squares
+def sum_of_squares(numbers):
+    # TODO: Implement this function
+    pass
+
+
+input6 = [1, 2, 3, 4]
+output6 = None  # TODO: Add your output here
+assert sum_of_squares(input6) == output6
+
+print("All exercises passed!")
